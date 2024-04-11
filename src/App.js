8d05261f0828d9dc396f13a38af2f74eafc1da4c
IT21307058 <IT21307058@my.sliit.lk>
2024-04-11 14:02:06 +0530

    add helmet commited

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import { Routes as Switch, Route, Routes } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Home  from "./pages/Home/Home"
+import Home from "./pages/Home/Home"
 import Header from "./components/Header/Header"
 import Fotter from "./components/Footer/Fotter";
+import { Helmet } from "react-helmet"
+import logo from '../src/assets/image/Logo.png'
 
 function App() {
   return (
     <Router>
-      <div style={{minHeight: "90vh", margin: "0px", padding: "0px"}}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>AT-Digital:#1 Digital Marketing Agency Sydney</title>
+      </Helmet>
+      <div style={{ minHeight: "90vh", margin: "0px", padding: "0px" }}>
         <Header />
         <Switch>
           <Route path="/" element={<Home />} />
           <Route path="/" element={<Home />} />
         </Switch>
       </div>
-      <Fotter/>
+      <Fotter />
     </Router>
   );
 }
